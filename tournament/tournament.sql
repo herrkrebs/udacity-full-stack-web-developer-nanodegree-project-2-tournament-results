f339627ae28744982e654f34b4011ccb8c48a59b
Kai Mies <develop@regnavi.de>
2016-03-09 11:00:29 +0100

    Initial commit

---
+++ tournament/tournament.sql
@@ -0,0 +1,28 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+
+-- Create the players table
+CREATE TABLE players (
+	id SERIAL PRIMARY KEY,
+	name text
+);
+
+-- Create the matches table
+CREATE TABLE matches (
+	winner SERIAL REFERENCES players(id),
+	loser SERIAL REFERENCES players(id)
+);
+
+-- Create view for player standings
+CREATE VIEW players_standings AS
+    SELECT players.id, players.name,
+        (SELECT COUNT(*) FROM matches WHERE matches.winner=players.id) AS wins,
+        (SELECT COUNT(*) FROM matches WHERE matches.winner=players.id OR matches.loser=players.id) AS matches
+    FROM players
+    ORDER BY wins DESC;
